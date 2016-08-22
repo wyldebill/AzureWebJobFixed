9b963b73c7692fc310b9356fe0a417177838b973
William Austin <william.austin@improving.com>
2016-08-22 13:10:06 -0500

    initial commit

---
+++ WebApplication12/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApplication12.Startup))]
+namespace WebApplication12
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ WebJob1/Functions.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using ContosoAdsCommon;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace WebJob1
+{
+    public class Functions
+    {
+        
+        public static void GenerateThumbnail(
+        [QueueTrigger("thumbnailrequest")] BlobInformation blobInfo,
+        [Blob("images/{BlobName}", FileAccess.Read)] Stream input,
+        [Blob("images/{BlobNameWithoutExtension}_thumbnail.jpg")] CloudBlockBlob outputBlob)
+        {
+            using (Stream output = outputBlob.OpenWrite())
+            {
+                ConvertImageToThumbnailJPG(input, output);
+                outputBlob.Properties.ContentType = "image/jpeg";
+            }
+
+            Console.WriteLine("add thumbnail image to ad in database");
+            // Entity Framework context class is not thread-safe, so it must
+            // be instantiated and disposed within the function.
+            using (ContosoAdsContext db = new ContosoAdsContext())
+            {
+                var id = blobInfo.AdId;
+                Ad ad = db.Ads.Find(id);
+                if (ad == null)
+                {
+                    throw new Exception(String.Format("AdId {0} not found, can't create thumbnail", id.ToString()));
+                }
+                ad.ThumbnailURL = outputBlob.Uri.ToString();
+                db.SaveChanges();
+                Console.WriteLine("Thumbnail associated to ad");
+            }
+        }
+
+        public static void ConvertImageToThumbnailJPG(Stream input, Stream output)
+        {
+            Console.WriteLine("converting to thumbnail...");
+            int thumbnailsize = 80;
+            int width;
+            int height;
+            var originalImage = new Bitmap(input);
+
+            if (originalImage.Width > originalImage.Height)
+            {
+                width = thumbnailsize;
+                height = thumbnailsize * originalImage.Height / originalImage.Width;
+            }
+            else
+            {
+                height = thumbnailsize;
+                width = thumbnailsize * originalImage.Width / originalImage.Height;
+            }
+
+            Bitmap thumbnailImage = null;
+            try
+            {
+                thumbnailImage = new Bitmap(width, height);
+
+                using (Graphics graphics = Graphics.FromImage(thumbnailImage))
+                {
+                    graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                    graphics.SmoothingMode = SmoothingMode.AntiAlias;
+                    graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+                    graphics.DrawImage(originalImage, 0, 0, width, height);
+                }
+
+                thumbnailImage.Save(output, ImageFormat.Jpeg);
+            }
+            finally
+            {
+                if (thumbnailImage != null)
+                {
+                    thumbnailImage.Dispose();
+                }
+            }
+
+            Console.WriteLine("done!");
+        }
+    
+
+        // This function will get triggered/executed when a new message is written 
+        // on an Azure Queue called queue.
+        public static void ProcessQueueMessage([QueueTrigger("queue")] string message, TextWriter log)
+        {
+            log.WriteLine(message);
+        }
+    }
+}
